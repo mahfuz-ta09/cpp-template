872e9e24e946b7a897618f023f0083806e019634
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-03 12:12:00 +0600

    Add files via upload
    first insertion

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define no cout<<"NO\n"
+#define yes cout<<"YES\n"
+#define vec(x) for(auto a:x)cout<<a<<" "
+using namespace std;
+
+void solve(){
+    ll n ,i , x;
+
+}
+
+int main(){
+    ll t;
+    t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
