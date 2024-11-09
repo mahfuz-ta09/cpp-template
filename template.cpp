74c48f38fddeae56e93c36ec42ee24a0b0bca113
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-09 22:33:31 +0600

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -2,18 +2,25 @@
 #define ll long long int
 #define no cout<<"NO\n"
 #define yes cout<<"YES\n"
+#define nl cout<<"\n"
 #define vec(x) for(auto a:x)cout<<a<<" "
 using namespace std;
 
 void solve(){
-    ll n ,i , x;
+    ll n ,i , m;
+
 
 }
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
     ll t;
-    t = 1;
+    t=1;
     //cin >> t;
+
     while(t--){
         solve();
     }
