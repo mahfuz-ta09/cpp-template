64c9105c48d89959c8771785f1ee7ef32592666c
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-10 22:49:17 +0600

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -1,30 +1,24 @@
 #include<bits/stdc++.h>
 #define ll long long int
-#define no cout<<"NO\n"
-#define yes cout<<"YES\n"
+#define no cout<<"NO\n";
+#define yes cout<<"YES\n";
 #define nl cout<<"\n"
-#define vec(x) for(auto a:x)cout<<a<<" "
+#define vec(x) for(auto a:x)cout<<a<<" ";cout<<"\n";
+#define srt(v) sort(v.begin(),v.end());
+#define iOS ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
 void solve(){
-    ll n ,i , m;
-
+    ll n,i,m,p;
 
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-
+    iOS
     ll t;
     t=1;
     //cin >> t;
-
-    while(t--){
-        solve();
-    }
-
+    while(t--)solve();
     return 0;
 }
 
